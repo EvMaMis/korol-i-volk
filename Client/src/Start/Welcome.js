99935c4c45c1692425bb0fb38bcfcfe32cbd59c7
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-02-17 12:02:17 +0300

    Start

---
+++ Client/src/Start/Welcome.js
@@ -0,0 +1,15 @@
+
+function Welcome() {
+  return (
+    <div className="Container__welcome">
+        <div>
+            <img href="#" alt="Welcome"></img>
+        </div>
+        <div className="Box__btn">
+          <button>Начать Приключение</button>
+        </div>
+    </div>
+  );
+}
+
+export default Welcome;
