5b30655231859b19e539ee54cb3df50412f5d89a
Максим Сын Отца Своего <noimxl1@gmail.com>
2024-02-17 13:54:25 +0300

    Welcome and Registration

--- Client/src/Start/Welcome.js
+++ Client/src/Start/Welcome.js
@@ -1,12 +1,29 @@
+import {
+  Route,
+  Routes,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 function Welcome() {
   return (
     <div className="Container__welcome">
-        <div>
+        <div className="Welcome__title">
+            <h1>Король и Волк</h1>
             <img href="#" alt="Welcome"></img>
         </div>
+        <div className="Welcome__descrip">
+          <p>
+            Описание
+          </p>
+        </div>
         <div className="Box__btn">
+        <NavLink to="/Log-in"  className="General__btn">
           <button>Начать Приключение</button>
+        </NavLink>
+        <NavLink to="/Sign-in"  className="General__btn">
+          <button>Войти</button>
+        </NavLink>
         </div>
     </div>
   );
